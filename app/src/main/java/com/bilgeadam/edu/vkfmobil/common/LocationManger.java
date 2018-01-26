339c3e091e79b8702990dbf05ca4ed77f95a63ed
umutedu <uboz@stu.okan.edu.tr>
2018-01-26 16:58:57 +0300

    tabs viewpager set top films

---
+++ app/src/main/java/com/bilgeadam/edu/vkfmobil/common/LocationManger.java
@@ -0,0 +1,230 @@
+package com.bilgeadam.edu.vkfmobil.common;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+
+
+import com.bilgeadam.edu.vkfmobil.R;
+
+import static android.content.Context.LOCATION_SERVICE;
+
+public class LocationManger implements LocationListener {
+    private final Context mContext;
+
+    // flag for GPS status
+    boolean isGPSEnabled = false;
+
+    // flag for network status
+    boolean isNetworkEnabled = false;
+
+    Location location; // location
+    double latitude; // latitude
+    double longitude; // longitude
+
+    // The minimum distance to change Updates in meters
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 0;
+
+    // The minimum time between updates in milliseconds
+    private static final long MIN_TIME_BW_UPDATES = 0;
+
+    // Declaring a Location Manager
+    protected LocationManager locationManager;
+
+    public LocationManger(Context context) {
+        this.mContext = context;
+        getLocation();
+
+    }
+
+    public Location getLocation() {
+        try {
+            if (ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return null;
+            }
+
+            locationManager = (LocationManager) mContext
+                    .getSystemService(LOCATION_SERVICE);
+
+            // getting GPS status
+            isGPSEnabled = locationManager
+                    .isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+            // getting network status
+            isNetworkEnabled = locationManager
+                    .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            if (!isGPSEnabled && !isNetworkEnabled) {
+                // no network provider is enabled
+            } else {
+                // First get location from Network Provider
+                if (isNetworkEnabled) {
+
+                    locationManager.requestLocationUpdates(
+                            LocationManager.NETWORK_PROVIDER,
+                            MIN_TIME_BW_UPDATES,
+                            MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                    Log.d("Network", "Network");
+                    if (locationManager != null) {
+                        location = locationManager
+                                .getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                        if (location != null) {
+                            latitude = location.getLatitude();
+                            longitude = location.getLongitude();
+                        }
+                    }
+                }
+                // if GPS Enabled get lat/long using GPS Services
+                if (isGPSEnabled) {
+                    if (location == null) {
+                        locationManager.requestLocationUpdates(
+                                LocationManager.GPS_PROVIDER,
+                                MIN_TIME_BW_UPDATES,
+                                MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                        Log.d("GPS Enabled", "GPS Enabled");
+                        if (locationManager != null) {
+                            location = locationManager
+                                    .getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                            if (location != null) {
+                                latitude = location.getLatitude();
+                                longitude = location.getLongitude();
+                            }
+                        }
+                    }
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return location;
+    }
+
+    /**
+     * Stop using GPS listener
+     * Calling this function will stop using GPS in your app
+     */
+    public void startUsingGPS() {
+        stopUsingGPS();
+        getLocation();
+
+    }
+
+    public void stopUsingGPS() {
+        if (locationManager != null) {
+            if (ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            locationManager.removeUpdates(this);
+        }
+    }
+
+    /**
+     * Function to get latitude
+     */
+    public double getLatitude() {
+        if (location != null) {
+            latitude = location.getLatitude();
+        }
+
+        // return latitude
+        return latitude;
+    }
+
+    /**
+     * Function to get longitude
+     */
+    public double getLongitude() {
+        if (location != null) {
+            longitude = location.getLongitude();
+        }
+
+        // return longitude
+        return longitude;
+    }
+
+    /**
+     * Function to check GPS/wifi enabled
+     *
+     * @return boolean
+     */
+    public boolean canGetLocation() {
+        if (locationManager != null) {
+            isGPSEnabled = locationManager
+                    .isProviderEnabled(LocationManager.GPS_PROVIDER);
+            isNetworkEnabled = locationManager
+                    .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+            if (isGPSEnabled == true || isNetworkEnabled == true) {
+                if (location == null) {
+                    startUsingGPS();
+                }
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Function to show settings alert dialog
+     * On pressing Settings button will lauch Settings Options
+     */
+    public void showSettingsAlert() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(mContext);
+
+        // Setting Dialog Title
+        alertDialog.setTitle(mContext.getString(R.string.gps_warnningtitle));
+
+        // Setting Dialog Message
+        alertDialog.setMessage(mContext.getString(R.string.gps_warnningmessage));
+
+        // On pressing Settings button
+        alertDialog.setPositiveButton(mContext.getString(R.string.gps_warnningsettingbtn), new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                mContext.startActivity(intent);
+            }
+        });
+
+        // on pressing cancel button
+        alertDialog.setNegativeButton(mContext.getString(R.string.gps_warnningcancelbtn), new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        // Showing Alert Message
+        alertDialog.show();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+    }
+
+
+}
--- app/src/main/java/com/bilgeadam/edu/vkfmobil/view/activity/MainActivity.java
+++ app/src/main/java/com/bilgeadam/edu/vkfmobil/view/activity/MainActivity.java
@@ -1,16 +1,24 @@
 package com.bilgeadam.edu.vkfmobil.view.activity;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
 
 import com.bilgeadam.edu.vkfmobil.R;
 import com.bilgeadam.edu.vkfmobil.common.Base.AndroidLogger;
+import com.bilgeadam.edu.vkfmobil.common.LocationManger;
 import com.bilgeadam.edu.vkfmobil.json.ModelParserJSON;
 import com.bilgeadam.edu.vkfmobil.listener.ServiceListener;
 import com.bilgeadam.edu.vkfmobil.model.Base.BaseServiceErrorModel;
@@ -30,6 +38,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
 
+    LocationManger locationManger;
     Toolbar toolbar;
     DrawerLayout drawerLayout;
     TabLayout tabLayout;
@@ -48,6 +57,18 @@ public class MainActivity extends AppCompatActivity {
         initViews();
 
         setTabs();
+
+        locationManger = new LocationManger(MainActivity.this);
+
+        if(checkPermission(MainActivity.this))
+        {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},1);
+        }
+        else
+        {
+            locationManger.startUsingGPS();
+        }
 
     }
     private void initViews() {
@@ -69,5 +90,39 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public static boolean checkPermission(final Context context) {
+        return ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        switch (requestCode)
+        {
+            case  1:{
+
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    locationManger.startUsingGPS();
+
+                    // TODO: 30/07/2017 map operations/
+                    //
+                    // goMapActivity();
+
+                } else {
+                    Toast.makeText(MainActivity.this, "izin yok", Toast.LENGTH_SHORT).show();
+                    // izin verilmedi
+                }
+                return;
+
+
+
+            }
+
+        }
+
+
+
+    }
 }
